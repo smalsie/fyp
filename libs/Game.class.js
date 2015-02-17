35c395fcf8c5cee051849113cc01ab42bff0dcbf
joshuahugh <joshuahugh94@gmail.com>
2015-02-17 22:14:11 +0000

    Tutorial 1 - Finished Documentation

--- libs/Game.class.js
+++ libs/Game.class.js
@@ -1,15 +1,38 @@
+/**
+* Game class which is at the core of the project
+* @author Joshua Small [smalljh@aston.ac.uk]
+* @version 1.0
+*
+* @constructor
+* @param {int} width Width of the game
+* @param {int} height Height of the game
+* @param {String} name The name of the game
+*/
 function Game(width, height,  name){
 
     document.title = name
     
     if (typeof phaserType === 'undefined') { phaserType = Phaser.AUTO; }
     
+    /** @member {Phaser.Game} */
     this.world = new Phaser.Game(width, height, Phaser.AUTO, '', { preload: preload, create: create, update: update });
     
+    /**
+	* Game Height function that returns the height of the game, useful when 
+	* a percentage is give as the size in pixels will be returned
+	*
+	* @return {Int} Returns the height of the game in pixels
+	*/
     this.gameHeight = function(){
         return this.world.world.height;
     };
-   
+    
+    /**
+	* Game Width function that returns the height of the game, useful when 
+	* a percentage is give as the size in pixels will be returned
+	*
+	* @return {Int} Returns the width of the game in pixels
+	*/   
     this.gameWidth = function(){
         return this.world.world.width;
     };
--- libs/Keys.class.js
+++ libs/Keys.class.js
@@ -1,10 +1,24 @@
+/**
+* Keys class, holds all of the possible keys available
+*
+* @author Joshua Small
+* @version 1.0
+*
+* @constructor
+* @param {Game} game The game object
+*/
 function Keys(game) {
+	/** @member {Phaser.Game} */
 	this.game = game.world;
-	
+	/** @member {Phaser.Key} */
 	this.phaserKey = this.game.input.keyboard.createCursorKeys();
+	/** @member {Phaser.Key} */
 	this.left = new Key(this.phaserKey.left);
+	/** @member {Phaser.Key} */
 	this.right = new Key(this.phaserKey.right);
+	/** @member {Phaser.Key} */
 	this.up = new Key(this.phaserKey.up);
+	/** @member {Phaser.Key} */
 	this.down = new Key(this.phaserKey.down);
 
 };
